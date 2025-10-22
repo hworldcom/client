91c2b2ba582883be3726678300417b065c291ec5
hworldcom <hoangworldcom@gmail.com>
2025-10-23 00:25:28 +0200

    add longer timeout

--- src/lnlabs_agent/scraper/web_crawler.py
+++ src/lnlabs_agent/scraper/web_crawler.py
@@ -761,7 +761,7 @@ class WebCrawler(SecureCookieMixin):
         await self.page.wait_for_timeout(1200)
         self.log("[step] open employees")
         await self._open_company_employees()
-        await self.page.wait_for_timeout(2000)
+        await self.page.wait_for_timeout(20000)
 
         self.log("[step] filter 2nd-degree (simple toolbar click)]")
         ok = await self._click_second_degree_simple(timeout_ms=15_000)
